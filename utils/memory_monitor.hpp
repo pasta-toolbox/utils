c34658f5b1fb0929a5ea7a97dbd878d50435940a
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2021-10-25 13:43:37 +0200

    Make memory monitor header-only

--- utils/memory_monitor.hpp
+++ utils/memory_monitor.hpp
@@ -20,6 +20,12 @@
 
 #pragma once
 
+#include <cstddef>
+#include <ostream>
+#include <iostream>
+
+#include "utils/malloc.hpp"
+
 namespace pasta {
 
   /*!
@@ -56,11 +62,22 @@ namespace pasta {
     MemoryMonitorStats(size_t const _cur_peak, size_t const _total_size_malloc,
 		       size_t const _total_size_free,
 		       size_t const _number_malloc,
-		       size_t const _number_free, size_t const _uncounted);
+		       size_t const _number_free, size_t const _uncounted)
+      : cur_peak(_cur_peak), total_size_malloc(_total_size_malloc),
+	total_size_free(_total_size_free), number_malloc(_number_malloc),
+	number_free(_number_free), uncounted(_uncounted) { }
   }; // struct MemoryMonitorStats
 
   //! Simple output of \c MemoryMonitorStats.
-  std::ostream& operator << (std::ostream&, const MemoryMonitorStats& mms);
+  std::ostream& operator << (std::ostream&, const MemoryMonitorStats& mms) {
+    os << "cur_peak=" << mms.cur_peak << " "
+       << "total_size_malloc=" << mms.total_size_malloc << " "
+       << "total_size_free=" << mms.total_size_free  << " "
+       << "number_malloc=" << mms.number_malloc << " "
+       << "number_free=" << mms.number_free << " "
+       << "uncounted=" << mms.uncounted;
+    return os;
+  }
 
   //! \addtogroup pasta_util
   //! \{
@@ -77,38 +94,67 @@ namespace pasta {
     MemoryMonitorStats memory_stats_;
 
     //! Private constructor to enable singleton pattern.
-    MemoryMonitor();
+    MemoryMonitor() : memory_stats_(0, 0, 0, 0, 0, 0) {
+      MemoryMonitorCallbacks::malloc_callback = MemoryMonitor::malloc_callback;
+      MemoryMonitorCallbacks::free_callback = MemoryMonitor::free_callback;
+      MemoryMonitorCallbacks::uncounted_callback =
+	MemoryMonitor::uncounted_callback;
+    }
 
     //! Callback for malloc (used in malloc.cpp).
-    static void malloc_callback(size_t const size);
+    static void malloc_callback(size_t const size) {
+      instance().memory_stats_.cur_peak += size;
+      instance().memory_stats_.total_size_malloc += size;
+      ++instance().memory_stats_.number_malloc;
+    }
+
     //! Callback for free (used in malloc.cpp)
-    static void free_callback(size_t const size);
+    static void free_callback(size_t const size) {
+      MemoryMonitorStats& cur_stats = instance().memory_stats_;
+      cur_stats.cur_peak -= size;
+      cur_stats.total_size_free += size;
+      ++cur_stats.number_free;
+    }
+
     //! Callback for malloc/free that are not tracked (which should not occur).
-    static void uncounted_callback();
+    static void uncounted_callback() {
+      ++instance().memory_stats_.uncounted;
+    }
 
   public:
     /*!
      * \brief Get the \c MemoryMonitor singleton instance.
      * \return Singleton instance of \c MemoryMonitor.
      */
-    static MemoryMonitor& instance();
+    static MemoryMonitor& instance() {
+      static MemoryMonitor instance;
+      return instance;
+    }
 
     //! Reset the current measurement. All measurements are set to zero.
-    void reset();
+    void reset() {
+      instance().memory_stats_ = { 0, 0, 0, 0, 0, 0 };
+    }
 
     /*!
      * \brief Get the current stats of the monitored memory.
      * \return Current stats of the monitored memory.
      */
     [[nodiscard]]
-    MemoryMonitorStats get();
+    MemoryMonitorStats get() {
+      return memory_stats_;
+    }
 
     /*!
      * \brief Get the current stats of the monitored memory. Then reset stats.
      * \return Current stats of the monitored memory.
      */
     [[nodiscard]]
-    MemoryMonitorStats get_and_reset();
+    MemoryMonitorStats get_and_reset() {
+      auto tmp = memory_stats_;
+      reset();
+      return tmp;
+    }
   }; // class MemoryMonitor
 
   //! \}
